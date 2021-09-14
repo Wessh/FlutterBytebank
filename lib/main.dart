7bcafefc71c2152566bac8836cfa7e7bf2bb71cf
Wessh <wesshbr@gmail.com>
2021-09-13 22:26:15 -0300

    Implementando_o_Design_do_Item_de_Transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('200.0'),
+                subtitle: Text('6840'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('Valor'),
+                subtitle: Text('Conta'),
+              ),
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
+          onPressed: () {},
           child: Icon(Icons.add),
         ),
       ),
