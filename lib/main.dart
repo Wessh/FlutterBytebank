f93294493a6fda6cce67030c1c8abed8781fa1e2
Wessh <wesshbr@gmail.com>
2021-09-13 23:06:13 -0300

    Refatorando_o_Codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.0'),
-                subtitle: Text('6840'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('Valor'),
-                subtitle: Text('Conta'),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencia(),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
@@ -29,3 +14,49 @@ void main() => runApp(MaterialApp(
         ),
       ),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  /*
+   * StatelessWidget = Conteudo de forma estatica
+   * StatefulWidget = Tem a capacidade de modificar itens de forma dinamica
+   */
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        ItemTransferencia(
+          Transferencia(100.2, 514),
+        ),
+        ItemTransferencia(
+          Transferencia(1300.2, 5144),
+        ),
+        ItemTransferencia(
+          Transferencia(4020.63, 6748),
+        ),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Transferencia {
+  late final double valor;
+  late final int numeroConta;
+  Transferencia(this.valor, this.numeroConta);
+}
